8d019e5cf49d94bce096a9537adc27dad038cf05
AnElectricalEngineer <47844685+AnElectricalEngineer@users.noreply.github.com>
2021-04-26 19:44:35 +0300

    Update ex1.cu

--- ex1.cu
+++ ex1.cu
@@ -1,16 +1,85 @@
 #include "ex1.h"
 
+#define HIST_LENGTH 256
+#define IMG_SIZE IMG_WIDTH*IMG_HEIGHT
+
 __device__ void prefix_sum(int arr[], int arr_size) {
-    return; // TODO
+//    /* using Kogge-Stone algorithm */    /* assuming num of threads >= arr_size */    int tid = threadIdx.x;    int increment;    for(int stride = 1; stride < arr_size; stride *=2){        if(tid < arr_size && tid >= stride){            increment = arr[tid - stride];        }        __syncthreads();        if(tid < arr_size && tid >= stride){            arr[tid] += increment;        }        __syncthreads();    }    return; // TODO: verify if support of arr_size > num_of_threads is needed}
+
+    int threadID = threadIdx.x;
+    int offset = 1;
+
+    for(int level = arr_size / 2; level > 0; level /= 2)
+    {
+    	__syncthreads(); //TODO change order later
+    	if(threadID < level)
+    	{
+
+        	printf("%d %d\n", offset * (2 * threadID + 2) - 1, offset * (2 * threadID + 1) - 1);
+    		arr[offset * (2 * threadID + 2) - 1] += arr[offset * (2 * threadID + 1) - 1];
+    	}
+    	offset *= 2;
+    }
+    if(threadID == 0)
+    {
+    	arr[arr_size - 1] = 0;
+    }
+
+    for(int level = 1; level < arr_size; level *= 2)
+    {
+    	offset *= 2;
+    	__syncthreads();
+    	if(threadID < level)
+    	{
+    		int temp = arr[offset * (2 * threadID + 1) - 1];
+    		arr[offset * (2 * threadID + 1) - 1] = arr[offset * (2 * threadID + 2) - 1];
+    		arr[offset * (2 * threadID + 2) - 1] += temp;
+    	}
+    }
+	return;
+
 }
 
 __global__ void process_image_kernel(uchar *all_in, uchar *all_out) {
-    return; // TODO
+    __shared__ int hist[HIST_LENGTH];
+
+    int threadID = threadIdx.x;
+    int blockSize = blockDim.x;
+
+    if(threadID < HIST_LENGTH)
+    {
+    	hist[threadID] = 0;
+    }
+
+    //	Create the histogram
+    for(int i = threadID; i < IMG_SIZE; i += blockSize)
+    {
+    	atomicAdd(&hist[all_in[i]], 1);
+    }
+    __syncthreads();
+
+    //	Create the CDF
+    prefix_sum(hist, HIST_LENGTH);
+    __syncthreads();
+
+    //	Create the map
+    if(threadID < HIST_LENGTH)
+    {
+        hist[threadID] = (HIST_LENGTH / N_COLORS) * (int)(N_COLORS * (float)hist[threadID] / (IMG_WIDTH * IMG_HEIGHT));
+    }
+    __syncthreads();
+
+    //	Compute the new image
+    for(int i = threadID; i < IMG_SIZE; i += blockSize)
+        {
+        	all_out[i] = hist[all_in[i]];
+        }
+	return;
 }
 
 /* Task serial context struct with necessary CPU / GPU pointers to process a single image */
 struct task_serial_context {
-    // TODO define task serial memory buffers
+    uchar *all_in, *all_out;
 };
 
 /* Allocate GPU memory for a single input image and a single output image.
@@ -20,7 +89,8 @@ struct task_serial_context *task_serial_init()
 {
     auto context = new task_serial_context;
 
-    //TODO: allocate GPU memory for a single input image and a single output image
+    CUDA_CHECK(cudaMalloc((void**)&context->all_in, IMG_SIZE*sizeof(uchar)));
+    CUDA_CHECK(cudaMalloc((void**)&context->all_out, IMG_SIZE*sizeof(uchar)));
 
     return context;
 }
@@ -29,16 +99,20 @@ struct task_serial_context *task_serial_init()
  * provided output host array */
 void task_serial_process(struct task_serial_context *context, uchar *images_in, uchar *images_out)
 {
-    //TODO: in a for loop:
-    //   1. copy the relevant image from images_in to the GPU memory you allocated
-    //   2. invoke GPU kernel on this image
-    //   3. copy output from GPU memory to relevant location in images_out_gpu_serial
+    for(int imageIdx = 0; imageIdx < N_IMAGES; imageIdx++)
+    {
+    	CUDA_CHECK(cudaMemcpy((void*)context->all_in, (void*)(images_in + imageIdx * IMG_SIZE), IMG_SIZE*sizeof(uchar), cudaMemcpyHostToDevice));
+    	process_image_kernel<<<1, 1024>>>(context->all_in, context->all_out);
+    	CUDA_CHECK(cudaDeviceSynchronize());
+    	CUDA_CHECK(cudaMemcpy((void*)(images_out + imageIdx * IMG_SIZE), (void*)context->all_out, IMG_SIZE*sizeof(uchar), cudaMemcpyDeviceToHost));
+    }
 }
 
 /* Release allocated resources for the task-serial implementation. */
 void task_serial_free(struct task_serial_context *context)
 {
-    //TODO: free resources allocated in task_serial_init
+    CUDA_CHECK(cudaFree((void*)context->all_in));
+    CUDA_CHECK(cudaFree((void*)context->all_out));
 
     free(context);
 }
